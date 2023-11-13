90128a996874f8fe5dbc9a7eb5d554453898fa40
pollyandhiscat <pollyandhiscat@outlook.com>
2023-11-12 19:45:15 -0800

    Add 'getCompleteSummary' function to 'Book', summarizing the book's various data elements

--- js/main.js
+++ js/main.js
@@ -7,18 +7,24 @@ function Book(title, author, page_count, year_published, checked_out) {
     this.page_count = page_count;
     this.year_published = year_published;
     this.checked_out = checked_out;
+    this.getCompleteSummary = function() {
+
+        let summary = `The book: ${title} by ${author} is ${page_count} pages long ${'\n'} and was published in the year ${year_published}.` + " " + (checked_out == true ? "It is currently checked out." : "It is currently not checked out.");
+    }
 }
 
 function add_book_to_library(book) {
 
     /*
 
-    Takes a book object and adds that
-    book to the library's book collection.
+    Takes a book object and adds it
+    to the library's book collection.
 
     */
 
     library_collection.push(book);
 
 }
+
+
 
